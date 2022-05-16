cb39a601265a1e7ca8c71898d75ed5ead71bacd7
ayisha.siddiqua <ayisha.siddiqua@accenture.com>
2022-05-16 09:44:54 +0530

    First GIT Commit

---
+++ src/main/java/com/training/configserverapp/ConfigserverappApplication.java
@@ -0,0 +1,13 @@
+package com.training.configserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/configserverapp/ConfigserverappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.configserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
